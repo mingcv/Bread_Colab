116a819892d9801e2a827796634efebaa60479db
Qiming <18002177060@163.com>
2022-06-04 10:46:22 +0800

    Update colab_utils.py

--- colab_utils.py
+++ colab_utils.py
@@ -1,3 +1,66 @@
+import errno
+import hashlib
+import os
+import re
+import shutil
+import sys
+import tempfile
+import torch
+import warnings
+import zipfile
+
+from urllib.request import urlopen, Request
+from urllib.parse import urlparse  # noqa: F401
+
+
+def download_url_to_file(url, dst, hash_prefix=None, progress=True):
+    file_size = None
+    # We use a different API for python2 since urllib(2) doesn't recognize the CA
+    # certificates in older Python
+    req = Request(url, headers={"User-Agent": "torch.hub"})
+    u = urlopen(req)
+    meta = u.info()
+    if hasattr(meta, 'getheaders'):
+        content_length = meta.getheaders("Content-Length")
+    else:
+        content_length = meta.get_all("Content-Length")
+    if content_length is not None and len(content_length) > 0:
+        file_size = int(content_length[0])
+
+    # We deliberately save it in a temp file and move it after
+    # download is complete. This prevents a local working checkpoint
+    # being overridden by a broken download.
+    dst = os.path.expanduser(dst)
+    dst_dir = os.path.dirname(dst)
+    f = tempfile.NamedTemporaryFile(delete=False, dir=dst_dir)
+
+    try:
+        if hash_prefix is not None:
+            sha256 = hashlib.sha256()
+        with tqdm(total=file_size, disable=not progress,
+                  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            while True:
+                buffer = u.read(8192)
+                if len(buffer) == 0:
+                    break
+                f.write(buffer)
+                if hash_prefix is not None:
+                    sha256.update(buffer)
+                pbar.update(len(buffer))
+
+        f.close()
+        if hash_prefix is not None:
+            digest = sha256.hexdigest()
+            if digest[:len(hash_prefix)] != hash_prefix:
+                raise RuntimeError('invalid hash value (expected "{}", got "{}")'
+                                   .format(hash_prefix, digest))
+        shutil.move(f.name, dst)
+    finally:
+        f.close()
+        if os.path.exists(f.name):
+            os.remove(f.name)
+
+
 def imshow(image, click_event=None, cmap=None):
     """
     Show an image at true scale
@@ -16,13 +79,13 @@ def imshow(image, click_event=None, cmap=None):
     ax = fig.add_axes([0, 0, 1, 1])
 
     ax.imshow(image, interpolation='none', cmap=cmap)
-    
+
     plt.axis('off')
     plt.show()
-    
+
     return fig, ax
-   
-    
+
+
 def get_sed_model_file():
     import os.path
     default_path = 'data/opencv_sed_model.yml.gz'
@@ -33,8 +96,8 @@ def get_sed_model_file():
         dl_path = './opencv_sed_model.yml.gz'
         request.urlretrieve('https://github.com/higra/Higra-Notebooks/raw/master/data/opencv_sed_model.yml.gz', dl_path)
         return dl_path
-    
-    
+
+
 def locate_resource(name):
     import os.path
     default_path = 'data/' + name
@@ -42,8 +105,8 @@ def locate_resource(name):
         return default_path
     else:
         return 'https://github.com/higra/Higra-Notebooks/raw/master/data/' + name
-    
-    
+
+
 def enable_plotly_in_cell():
     """
     To be used in colab: this method pre-populates the outputframe with the configuration that Plotly expects 
@@ -58,21 +121,21 @@ def enable_plotly_in_cell():
     '''))
     init_notebook_mode(connected=False)
 
-    
+
 def saturate_max(array, max_saturation=0.005, normalize=True):
     import numpy as np
     values = np.ravel(array)
-    
+
     sorted_values = np.sort(values)
     max_v = sorted_values[min(len(sorted_values) - 1, int((1 - max_saturation) * len(sorted_values)))]
     saturated_im = array.copy()
     saturated_im[saturated_im > max_v] = max_v
-    
+
     if normalize:
         minv = np.min(saturated_im)
         maxv = np.max(saturated_im)
-        saturated_im = (saturated_im - minv) / (maxv - minv) 
-        
+        saturated_im = (saturated_im - minv) / (maxv - minv)
+
     return saturated_im
 
 
@@ -85,7 +148,7 @@ def get_tile_images(image, width, height):
     nrows, _m = divmod(_nrows, height)
     ncols, _n = divmod(_ncols, width)
     if _m != 0 or _n != 0:
-        image = image[:-_m,:-_n]
+        image = image[:-_m, :-_n]
 
     return np.lib.stride_tricks.as_strided(
         np.ravel(image),
